0a187fdc88a9bd6e259c8230da078b3d21e4dccf
Jonathan Helmus <jjhelmus@gmail.com>
2017-01-29 18:20:40 -0600

    Slicer Abstract base class

--- altslice.py
+++ altslice.py
@@ -1,14 +1,13 @@
 
+import abc
 import bisect
 
 
-class SequenceSlicer(object):
+class Slicer(metaclass=abc.ABCMeta):
 
-    def __init__(self, sequence):
-        self._sequence = sequence
-
-    def _transform_index(self, index):
-        return bisect.bisect_left(self._sequence, index)
+    @abc.abstractmethod
+    def _transform_index(self, x):
+        pass
 
     def __getitem__(self, x):
         """ Return an int/slice. """
@@ -27,7 +26,16 @@ class SequenceSlicer(object):
         return self.__getitem__(slice(*args))
 
 
-class UniformSlicer(object):
+class SequenceSlicer(Slicer):
+
+    def __init__(self, sequence):
+        self._sequence = sequence
+
+    def _transform_index(self, index):
+        return bisect.bisect_left(self._sequence, index)
+
+
+class UniformSlicer(Slicer):
 
     def __init__(self, start, step):
         self._start = start
@@ -36,24 +44,8 @@ class UniformSlicer(object):
     def _transform_index(self, index):
         return int((index - self._start) // (self._step))
 
-    def __getitem__(self, x):
-        """ Return an int/slice. """
-        if isinstance(x, slice):
-            start = stop = step = None
-            if x.start is not None:
-                start = self._transform_index(x.start)
-            if x.stop is not None:
-                stop = self._transform_index(x.stop)
-            step = x.step  # step is not transformed
-            return slice(start, stop, step)
-        else:  # single element
-            return self._transform_index(x)
 
-    def __call__(self, *args):
-        return self.__getitem__(slice(*args))
-
-
-class CategoricalSlicer(object):
+class CategoricalSlicer(Slicer):
 
     def __init__(self, categories):
         if hasattr(categories, 'index'):
@@ -63,19 +55,3 @@ class CategoricalSlicer(object):
 
     def _transform_index(self, index):
         return self._categories.index(index)
-
-    def __getitem__(self, x):
-        """ Return an int/slice. """
-        if isinstance(x, slice):
-            start = stop = step = None
-            if x.start is not None:
-                start = self._transform_index(x.start)
-            if x.stop is not None:
-                stop = self._transform_index(x.stop)
-            step = x.step  # step is not transformed
-            return slice(start, stop, step)
-        else:  # single element
-            return self._transform_index(x)
-
-    def __call__(self, *args):
-        return self.__getitem__(slice(*args))
